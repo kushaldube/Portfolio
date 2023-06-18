35ff8d044b06d8cbd6e258a2bd4705a025c93c97
Kushal Dube <kushaldube02@gmail.com>
2023-06-19 01:47:59 +0530

    initial commit

---
+++ src/component/Footer.js
@@ -0,0 +1,20 @@
+import React from "react"
+import logo1 from "./pic/f_logo.png"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='conatiner text-center top'>
+          <div className='img'>
+            {/* <img src={logo1} alt='' /> */}
+            <h1>KUSHAL DUBE - PORTFOLIO</h1>
+          </div>
+          <p>© 2023. All rights reserved by kushaldube.</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/component/Resume/ResumeApi.js
@@ -0,0 +1,52 @@
+const ResumeApi = [
+  {
+    id: 1,
+    category: "education",
+    year: "VIT Bhopal University (2020 - 2024)",
+    title: "B.Tech. Computer Science & Engineering",
+    // desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "8.25/10",
+  },
+  {
+    id: 2,
+    category: "education",
+    year: "Rajhans Vidyalaya (2018 - 2020) ",
+    title: "Higer Secondary - 12th (CBSE)",
+    // desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "82.80%",
+  },
+  {
+    id: 3,
+    category: "education",
+    year: "Jankidevi Public School (2007 - 2018) ",
+    title: "Elementary - 10th (CISCE)",
+    // desc: "If you are going to use a passage. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "4.80/5 ",
+  },
+  {
+    id: 4,
+    category: "experience",
+    year: "Coursera (Ongoing) ",
+    title: "Google UX Design Professional Certificate",
+    desc: "Contrary to popular belief. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "00.00%",
+  },
+  {
+    id: 5,
+    category: "experience",
+    year: "by Angels Yu, Udemy (Ongoing) ",
+    title: "Web Development Bootcamp",
+    desc: "Generate Lorem Ipsum which looks. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "00.00%",
+  },
+  {
+    id: 6,
+    category: "experience",
+    year: "Microsoft (May 2020) ",
+    title: "Microsoft Office Specialist",
+    desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+    rate: "00.00%",
+  },
+]
+
+export default ResumeApi
